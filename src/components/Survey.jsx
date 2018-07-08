7f81066bded1e14aae75ad12a42189374a94c81d
MuhammadRamadiansyah <ramadiansyah@hotmail.com>
2018-07-08 08:52:16 +0700

    add animation on react-vis

--- src/components/Survey.jsx
+++ src/components/Survey.jsx
@@ -18,12 +18,30 @@ class Survey extends Component {
                 <YAxis />
                 <XAxis />
                 <VerticalBarSeries
-                   data={[
+                    color="#79C7E3"
+                    onSeriesMouseOver={({ event })=>{
+                      let target = event.target
+                      target.style.transition = "0.8s"
+                      target.style.fill = "#2979FF"
+                      target.style.transform = "translate(0, -20px)"
+
+                    }}
+                    onSeriesMouseOut={({ event })=>{
+                      let target = event.target
+                      target.style.fill = "#79C7E3"
+                      target.style.transform = "translate(0, 0)"
+                    }}
+                    data={[
                     {x: 'Mountain', y: 3},
                     {x: 'Sea', y: 7},
                     {x: 'Beach', y: 2},
-                    {x: 'Old Town', y: 1}
-                  ]}/>
+                    {x: 'Old Town', y: 1},
+                    {x: 'City', y: 5},
+                    {x: 'Theatre', y: 8},
+                    {x: 'Young Town', y: 9}
+                    ]}
+                    stroke={'#ddd'}
+                    strokeWidth={2}/>
             </XYPlot>
       </div>
     );
